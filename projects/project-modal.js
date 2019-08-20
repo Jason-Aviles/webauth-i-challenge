1fa1eae59a8820ae295f0bb024c0863ef5de693b
Jason Aviles <sandman2k18@outlook.com>
2019-08-20 14:47:55 -0400

    helper function

---
+++ projects/project-modal.js
@@ -0,0 +1,22 @@
+const knex = require("knex");
+const knexConfig = require("../knexfile");
+const db = knex(knexConfig.development);
+
+module.exports = {
+  find,
+add,findBy
+};
+
+
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+
+function add(item) {
+  return db("users")
+    .insert(item)
+    .then(ids => ({ id: ids[0] }));
+}
+
